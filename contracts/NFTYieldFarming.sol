b26a48d35418243d03ffca96eed457ed00ec7e94
masaun <masanoriuno@gmail.com>
2021-02-13 11:07:44 +0900

    Import IERC1155 and IERC20 from @openzeppelin

--- contracts/NFTYieldFarming.sol
+++ contracts/NFTYieldFarming.sol
@@ -1,3 +1,12 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.6.12;
+
+import { IERC1155 } from '@openzeppelin/contracts/token/ERC1155/IERC1155.sol';
+import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import { SafeMath } from '@openzeppelin/contracts/math/SafeMath.sol';
+import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
+
+
 contract NFTYieldFarming is Ownable {
     using SafeMath for uint256;
     
