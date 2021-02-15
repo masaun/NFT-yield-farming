185f04f92758f2f2e1a7d936b5a356d0dec9d283
masaun <masanoriuno@gmail.com>
2021-02-15 10:21:45 +0900

    Define new NFTYieldFarming.sol and GovernanceToken.sol

--- contracts/NFTYieldFarming.sol
+++ contracts/NFTYieldFarming.sol
@@ -1,123 +1,278 @@
-// SPDX-License-Identifier: agpl-3.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.6.12;
 
-import { IERC721 } from '@openzeppelin/contracts/token/ERC721/IERC721.sol';
-import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import { SafeMath } from '@openzeppelin/contracts/math/SafeMath.sol';
-import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/EnumerableSet.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./SushiToken.sol";
 
-
-/**
- * @notice - This is the NFT Yield Fariming contract (for ERC721-based NFT)
- */
+// NFTYieldFarming is the master of Sushi. He can make Sushi and he is a fair guy.
 contract NFTYieldFarming is Ownable {
     using SafeMath for uint256;
-    
+    using SafeERC20 for IERC20;
+
+    // Info of each user.
     struct UserInfo {
-        uint256 amount;         // current staked LP
-        uint256 lastUpdateAt;   // unix timestamp for last details update (when points calculated)
-        uint256 points;         // total points collected before latest deposit
+        uint256 amount;     // How many LP tokens the user has provided.
+        uint256 rewardDebt; // Reward debt. See explanation below.
+    }
+
+    // Info of each pool.
+    struct PoolInfo {
+        IERC20 lpToken; // Address of LP token contract.
+        uint256 allocPoint; // How many allocation points assigned to this pool. SUSHIs to distribute per block.
+        uint256 lastRewardBlock; // Last block number that SUSHIs distribution occurs.
+        uint256 accSushiPerShare; // Accumulated SUSHIs per share, times 1e12. See below.
     }
     
-    struct NFTInfo {
-        address contractAddress;
-        uint256 remaining;      // NFTs remaining to farm
-        uint256 price;          // points required to claim NFT
+    // The SUSHI TOKEN!
+    SushiToken public sushi;
+    
+    // Dev address.
+    address public devaddr;
+    
+    // Block number when bonus SUSHI period ends.
+    uint256 public bonusEndBlock;
+    
+    // SUSHI tokens created per block.
+    uint256 public sushiPerBlock;
+    
+    // Bonus muliplier for early sushi makers.
+    uint256 public constant BONUS_MULTIPLIER = 10;
+    
+    // The migrator contract. It has a lot of power. Can only be set through governance (owner).
+    IMigratorChef public migrator;
+    
+    // Info of each pool.
+    PoolInfo[] public poolInfo;
+    
+    // Info of each user that stakes LP tokens.
+    mapping(uint256 => mapping(address => UserInfo)) public userInfo;
+    
+    // Total allocation poitns. Must be the sum of all allocation points in all pools.
+    uint256 public totalAllocPoint = 0;
+    
+    // The block number when SUSHI mining starts.
+    uint256 public startBlock;
+    event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
+    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event EmergencyWithdraw(
+        address indexed user,
+        uint256 indexed pid,
+        uint256 amount
+    );
+
+    constructor(
+        SushiToken _sushi,
+        address _devaddr,
+        uint256 _sushiPerBlock,
+        uint256 _startBlock,
+        uint256 _bonusEndBlock
+    ) public {
+        sushi = _sushi;
+        devaddr = _devaddr;
+        sushiPerBlock = _sushiPerBlock;
+        bonusEndBlock = _bonusEndBlock;
+        startBlock = _startBlock;
     }
-    
-    uint256 public emissionRate;  // points generated per LP token per second staked
-    IERC20 lpToken;               // token being staked
-    
-    NFTInfo[] public nftInfo;
-    mapping(address => UserInfo) public userInfo;
-    
-    constructor(uint256 _emissionRate, IERC20 _lpToken) public {
-        emissionRate = _emissionRate;
-        lpToken = _lpToken;
+
+    function poolLength() external view returns (uint256) {
+        return poolInfo.length;
     }
-    
-    /**
-     * @notice - Add a new NFT token to the Pool (as a target to stake). Can only be called by the owner.
-     * @notice - XXX DO NOT add the same NFT token more than once. Rewards will be messed up if you do.
-     */
-    function addNFT(
-        address contractAddress,    // Only ERC721 NFT Supported!
-        uint256 total,              // amount of NFTs deposited to farm (need to approve before)
-        uint256 price
-    ) external onlyOwner {
-        nftInfo.push(NFTInfo({
-            contractAddress: contractAddress,  /// ERC721 NFT contract address
-            remaining: total,
-            price: price
-        }));
+
+    // Add a new lp to the pool. Can only be called by the owner.
+    // XXX DO NOT add the same LP token more than once. Rewards will be messed up if you do.
+    function add(
+        uint256 _allocPoint,
+        IERC20 _lpToken,
+        bool _withUpdate
+    ) public onlyOwner {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        uint256 lastRewardBlock =
+            block.number > startBlock ? block.number : startBlock;
+        totalAllocPoint = totalAllocPoint.add(_allocPoint);
+        poolInfo.push(
+            PoolInfo({
+                lpToken: _lpToken,
+                allocPoint: _allocPoint,
+                lastRewardBlock: lastRewardBlock,
+                accSushiPerShare: 0
+            })
+        );
     }
-    
-    function stake(uint256 _amount) external {
-        lpToken.transferFrom(
-            msg.sender,
+
+    // Update the given pool's SUSHI allocation point. Can only be called by the owner.
+    function set(
+        uint256 _pid,
+        uint256 _allocPoint,
+        bool _withUpdate
+    ) public onlyOwner {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        totalAllocPoint = totalAllocPoint.sub(poolInfo[_pid].allocPoint).add(
+            _allocPoint
+        );
+        poolInfo[_pid].allocPoint = _allocPoint;
+    }
+
+    // Set the migrator contract. Can only be called by the owner.
+    function setMigrator(IMigratorChef _migrator) public onlyOwner {
+        migrator = _migrator;
+    }
+
+    // Migrate lp token to another lp contract. Can be called by anyone. We trust that migrator contract is good.
+    function migrate(uint256 _pid) public {
+        require(address(migrator) != address(0), "migrate: no migrator");
+        PoolInfo storage pool = poolInfo[_pid];
+        IERC20 lpToken = pool.lpToken;
+        uint256 bal = lpToken.balanceOf(address(this));
+        lpToken.safeApprove(address(migrator), bal);
+        IERC20 newLpToken = migrator.migrate(lpToken);
+        require(bal == newLpToken.balanceOf(address(this)), "migrate: bad");
+        pool.lpToken = newLpToken;
+    }
+
+    // Return reward multiplier over the given _from to _to block.
+    function getMultiplier(uint256 _from, uint256 _to)
+        public
+        view
+        returns (uint256)
+    {
+        if (_to <= bonusEndBlock) {
+            return _to.sub(_from).mul(BONUS_MULTIPLIER);
+        } else if (_from >= bonusEndBlock) {
+            return _to.sub(_from);
+        } else {
+            return
+                bonusEndBlock.sub(_from).mul(BONUS_MULTIPLIER).add(
+                    _to.sub(bonusEndBlock)
+                );
+        }
+    }
+
+    // View function to see pending SUSHIs on frontend.
+    function pendingSushi(uint256 _pid, address _user)
+        external
+        view
+        returns (uint256)
+    {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        uint256 accSushiPerShare = pool.accSushiPerShare;
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (block.number > pool.lastRewardBlock && lpSupply != 0) {
+            uint256 multiplier =
+                getMultiplier(pool.lastRewardBlock, block.number);
+            uint256 sushiReward =
+                multiplier.mul(sushiPerBlock).mul(pool.allocPoint).div(
+                    totalAllocPoint
+                );
+            accSushiPerShare = accSushiPerShare.add(
+                sushiReward.mul(1e12).div(lpSupply)
+            );
+        }
+        return user.amount.mul(accSushiPerShare).div(1e12).sub(user.rewardDebt);
+    }
+
+    // Update reward vairables for all pools. Be careful of gas spending!
+    function massUpdatePools() public {
+        uint256 length = poolInfo.length;
+        for (uint256 pid = 0; pid < length; ++pid) {
+            updatePool(pid);
+        }
+    }
+
+    // Update reward variables of the given pool to be up-to-date.
+    function updatePool(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        if (block.number <= pool.lastRewardBlock) {
+            return;
+        }
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (lpSupply == 0) {
+            pool.lastRewardBlock = block.number;
+            return;
+        }
+        uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+        uint256 sushiReward =
+            multiplier.mul(sushiPerBlock).mul(pool.allocPoint).div(
+                totalAllocPoint
+            );
+        sushi.mint(devaddr, sushiReward.div(10));
+        sushi.mint(address(this), sushiReward);
+        pool.accSushiPerShare = pool.accSushiPerShare.add(
+            sushiReward.mul(1e12).div(lpSupply)
+        );
+        pool.lastRewardBlock = block.number;
+    }
+
+    // Deposit LP tokens to MasterChef for SUSHI allocation.
+    function deposit(uint256 _pid, uint256 _amount) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        updatePool(_pid);
+        if (user.amount > 0) {
+            uint256 pending =
+                user.amount.mul(pool.accSushiPerShare).div(1e12).sub(
+                    user.rewardDebt
+                );
+            safeSushiTransfer(msg.sender, pending);
+        }
+        pool.lpToken.safeTransferFrom(
+            address(msg.sender),
             address(this),
             _amount
         );
-        
-        UserInfo storage user = userInfo[msg.sender];
-        
-        // already deposited before
-        if(user.amount != 0) {
-            user.points = earned(msg.sender);
-        }
         user.amount = user.amount.add(_amount);
-        user.lastUpdateAt = now;
+        user.rewardDebt = user.amount.mul(pool.accSushiPerShare).div(1e12);
+        emit Deposit(msg.sender, _pid, _amount);
     }
-    
-    // claim nft if points threshold reached
-    function claim(uint256 _nftIndex, uint256 _quantity) public {
-        NFTInfo storage nft = nftInfo[_nftIndex];
-        require(nft.remaining > 0, "All NFTs farmed");
-        require(earned(msg.sender) >= nft.price.mul(_quantity), "Insufficient Points");
-        UserInfo storage user = userInfo[msg.sender];
-        
-        // deduct points
-        user.points = earned(msg.sender).sub(nft.price.mul(_quantity));
-        user.lastUpdateAt = now;
-        
-        nft.remaining = nft.remaining.sub(_quantity);
-    }
-    
-    /// Withdraw parts of LP tokens amount
-    function withdraw(uint256 _amount) public {
-        UserInfo storage user = userInfo[msg.sender];
-        require(user.amount >= _amount, "Insufficient staked");
-        
-        // update userInfo
-        user.points = earned(msg.sender);
+
+    // Withdraw LP tokens from MasterChef.
+    function withdraw(uint256 _pid, uint256 _amount) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        require(user.amount >= _amount, "withdraw: not good");
+        updatePool(_pid);
+        uint256 pending =
+            user.amount.mul(pool.accSushiPerShare).div(1e12).sub(
+                user.rewardDebt
+            );
+        safeSushiTransfer(msg.sender, pending);
         user.amount = user.amount.sub(_amount);
-        user.lastUpdateAt = now;
-        
-        lpToken.transfer(
-            msg.sender,
-            _amount
-        );
-    }
-    
-    // withdraw all LP tokens
-    function exit() external {
-        withdraw(userInfo[msg.sender].amount);
-    }
-    
-    function earned(address userAddress) public view returns (uint256) {
-        UserInfo memory user = userInfo[userAddress];
-        return user.points.add(pending(user));
-    }
-    
-    function pending(UserInfo memory user) internal view returns (uint256) {
-        uint256 blockTime = block.timestamp;
-        return blockTime.sub(user.lastUpdateAt).mul(emissionRate).mul(user.amount);
- 
-    }
-    
-    function nftPoolLength() public view returns (uint256) {
-        return nftInfo.length;
+        user.rewardDebt = user.amount.mul(pool.accSushiPerShare).div(1e12);
+        pool.lpToken.safeTransfer(address(msg.sender), _amount);
+        emit Withdraw(msg.sender, _pid, _amount);
     }
 
+    // Withdraw without caring about rewards. EMERGENCY ONLY.
+    function emergencyWithdraw(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        pool.lpToken.safeTransfer(address(msg.sender), user.amount);
+        emit EmergencyWithdraw(msg.sender, _pid, user.amount);
+        user.amount = 0;
+        user.rewardDebt = 0;
+    }
 
+    // Safe sushi transfer function, just in case if rounding error causes pool to not have enough SUSHIs.
+    function safeSushiTransfer(address _to, uint256 _amount) internal {
+        uint256 sushiBal = sushi.balanceOf(address(this));
+        if (_amount > sushiBal) {
+            sushi.transfer(_to, sushiBal);
+        } else {
+            sushi.transfer(_to, _amount);
+        }
+    }
+
+    // Update dev address by the previous dev.
+    function dev(address _devaddr) public {
+        require(msg.sender == devaddr, "dev: wut?");
+        devaddr = _devaddr;
+    }
 }
