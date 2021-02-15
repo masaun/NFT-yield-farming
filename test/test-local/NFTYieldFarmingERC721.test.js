7922c5bf30ee80d3fa0ed8c74eef8627cc72890b
masaun <masanoriuno@gmail.com>
2021-02-15 12:00:25 +0900

    Add ./contracts/archive

--- test/test-local/NFTYieldFarmingERC721.test.js
+++ test/test-local/NFTYieldFarmingERC721.test.js
@@ -1,3 +1,9 @@
+///----------------------------
+/// Archive contract's test
+///----------------------------
+
+
+
 /// Using local network
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'));
