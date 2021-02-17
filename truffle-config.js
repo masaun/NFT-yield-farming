736bd08fb9308b30bf1782f518df192dc6f782c8
masaun <masanoriuno@gmail.com>
2021-02-17 09:46:41 +0900

    Define BSC-testnet in the truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -5,6 +5,13 @@ const mnemonic = process.env.MNEMONIC;
 
 module.exports = {
   networks: {
+    bsc: {  /// Binance Smart Chain testnet
+      provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
     ropsten: {
       provider: () => new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/' + process.env.INFURA_KEY),
       network_id: '3',
