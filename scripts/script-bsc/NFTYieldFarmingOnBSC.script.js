79f12a71acd32d4764484cd2ad5e739d50d80f01
masaun <masanoriuno@gmail.com>
2021-02-21 10:46:00 +0900

    [Script]: Math.round() + web3.utils.fromWei() for GovernanceToken balance

--- scripts/script-bsc/NFTYieldFarmingOnBSC.script.js
+++ scripts/script-bsc/NFTYieldFarmingOnBSC.script.js
@@ -204,27 +204,31 @@ async function totalSupplyOfGovernanceToken() {
     ///         - TotalSupply of GovernanceToken: 1000 * (321 - 310) = 11000
     ///         - A staker should have: 4*1000 + 4*1/3*1000 + 2*1/6*1000 = 5666
     ///         - NFTYieldFarming contract should have the remaining: 10000 - 5666 = 4334
-    let totalSupplyOfGovernanceToken = await governanceToken.totalSupply();
+    let _totalSupplyOfGovernanceToken = await governanceToken.totalSupply();
+    const totalSupplyOfGovernanceToken = Math.round(web3.utils.fromWei(String(_totalSupplyOfGovernanceToken), 'ether'));
     const currentBlock = await getCurrentBlock();
-    console.log(`Total Supply of the GovernanceToken should be ${String(totalSupplyOfGovernanceToken)} (at block ${currentBlock})`);
+    console.log(`Total Supply of the GovernanceToken should be ${totalSupplyOfGovernanceToken} (at block ${currentBlock})`);
 }
 
 async function governanceTokenBalanceOfStaker() {
-    let governanceTokenBalanceOfDeployer = await governanceToken.balanceOf(deployer, { from: deployer });
+    let _governanceTokenBalanceOfDeployer = await governanceToken.balanceOf(deployer, { from: deployer });
+    const governanceTokenBalanceOfDeployer = Math.round(web3.utils.fromWei(String(_governanceTokenBalanceOfDeployer), 'ether'));
     const currentBlock = await getCurrentBlock();
-    console.log(`GovernanceToken balance of a staker should be ${String(governanceTokenBalanceOfDeployer)} (at block ${currentBlock})`);
+    console.log(`GovernanceToken balance of a staker should be ${governanceTokenBalanceOfDeployer} (at block ${currentBlock})`);
 }
 
 async function governanceTokenBalanceOfAdmin() {
-    let governanceTokenBalanceOfAdmin = await governanceToken.balanceOf(admin, { from: admin });
+    let _governanceTokenBalanceOfAdmin = await governanceToken.balanceOf(admin, { from: admin });
+    const governanceTokenBalanceOfAdmin = Math.round(web3.utils.fromWei(String(_governanceTokenBalanceOfAdmin), 'ether'));
     const currentBlock = await getCurrentBlock();
-    console.log(`GovernanceToken balance of admin should be ${String(governanceTokenBalanceOfAdmin)} (at block ${currentBlock})`);
+    console.log(`GovernanceToken balance of admin should be ${governanceTokenBalanceOfAdmin} (at block ${currentBlock})`);
 }
 
 async function governanceTokenBalanceOfNFTYieldFarmingOnBSCContract() {
-    let governanceTokenBalance = await governanceToken.balanceOf(NFT_YIELD_FARMING, { from: deployer });
+    let _governanceTokenBalance = await governanceToken.balanceOf(NFT_YIELD_FARMING, { from: deployer });
+    const governanceTokenBalance = Math.round(web3.utils.fromWei(String(_governanceTokenBalance), 'ether'));
     const currentBlock = await getCurrentBlock();
-    console.log(`GovernanceToken balance of the NFTYieldFarmingOnBSC contract should be ${String(governanceTokenBalance)} (at block ${currentBlock})`);
+    console.log(`GovernanceToken balance of the NFTYieldFarmingOnBSC contract should be ${governanceTokenBalance} (at block ${currentBlock})`);
 }
 
 async function unstakeAndWithdraw() {
@@ -235,7 +239,8 @@ async function unstakeAndWithdraw() {
     let txReceipt = await nftYieldFarming.withdraw(_nftPoolId, _unStakeAmount, { from: deployer });
 
     /// Check final GovernanceToken balance of a staker
-    let governanceTokenBalanceOfDeployer = await governanceToken.balanceOf(deployer, { from: deployer });
+    let _governanceTokenBalanceOfDeployer = await governanceToken.balanceOf(deployer, { from: deployer });
+    const governanceTokenBalanceOfDeployer = Math.round(web3.utils.fromWei(String(_governanceTokenBalanceOfDeployer), 'ether'));
     const currentBlock = await getCurrentBlock();
-    console.log(`Un-stake (withdraw) 10 LP tokens and receive ${String(governanceTokenBalanceOfDeployer)} GovernanceToken as rewards (at block ${currentBlock})`);
+    console.log(`Un-stake (withdraw) 10 LP tokens and receive ${governanceTokenBalanceOfDeployer} GovernanceToken as rewards (at block ${currentBlock})`);
 }
